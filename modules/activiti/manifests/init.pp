eb2e4cd1fe75289008ed8ce83509afe4de6fa6bf
Bellraj Eapen <github@gulfdoctor.net>
2016-03-11 17:34:32 -0500

    Rest and mysql

--- modules/activiti/manifests/init.pp
+++ modules/activiti/manifests/init.pp
@@ -1,9 +1,14 @@
 class activiti {
   $version = '5.19.0.2'
+  $version_mysql = '5.1.38'
   $target = '/var/lib/tomcat7/webapps/activiti-explorer.war'
+  $target_rest = '/var/lib/tomcat7/webapps/activiti-rest.war'
 
   $uri = "https://github.com/Activiti/Activiti/releases/download/${version}/activiti-${version}.zip"
+  $uri_mysql = "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-${version_mysql}.zip"
 
+  $db_name=activiti
+  $db_password=mysql
 
   exec { "wget_activiti":
     path => "/bin:/usr/bin",
@@ -11,11 +16,24 @@ class activiti {
     command => "wget $uri -qO /home/vagrant/activiti.zip",
   }
 
+  exec { "wget_mysql":
+    path => "/bin:/usr/bin",
+    timeout     => 1800,
+    command => "wget $uri_mysql -qO /home/vagrant/mysql.zip",
+  }
+
   exec { "unzip_activiti":
     subscribe => Exec["wget_activiti"],
     refreshonly => true,
     path => "/bin:/usr/bin",
     command => "unzip /home/vagrant/activiti.zip -d /home/vagrant",
+  }
+
+  exec { "unzip_mysql":
+    subscribe => Exec["wget_mysql"],
+    refreshonly => true,
+    path => "/bin:/usr/bin",
+    command => "unzip /home/vagrant/mysql.zip -d /home/vagrant",
   }
 
   file { '/var/lib/tomcat7/webapps':
@@ -32,6 +50,26 @@ class activiti {
     command => "mv /home/vagrant/activiti-${version}/wars/activiti-explorer.war $target",
   }
 
+  exec { "move_activiti_rest":
+    subscribe => Exec["unzip_activiti"],
+    refreshonly => true,
+    path => "/bin:/usr/bin",
+    command => "mv /home/vagrant/activiti-${version}/wars/activiti-rest.war $target_rest",
+  }
+
+  exec { "move_mysql":
+    subscribe => Exec["unzip_mysql"],
+    refreshonly => true,
+    path => "/bin:/usr/bin",
+    command => "mv /home/vagrant/mysql-connector-java-${version_mysql}/mysql-connector-java-${version_mysql}-bin.jar code/mysql-connector-java-bin.jar",
+  }
+
+  exec { "mysql_create_db":
+    subscribe => Exec["move_activiti"],
+    refreshonly => true,
+    path => "/bin:/usr/bin",
+    command => "mysqladmin -uroot -p$db_password create $db_name",
+  }
 
 }
 
